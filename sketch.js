321da589efd3fa79cbb6ef72def8f15463b08a1a
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2022-11-03 15:45:56 -0300

    adicao personagem

--- sketch.js
+++ sketch.js
@@ -3,18 +3,19 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var engine, world, backgroundImg;
-var pirataAndando = [];
-var pirataAndandoJSON, pirataAndandoIMG;
+var pirataAtacando, pirataAndandoIMG;
 var chao;
-var esqueletoAndando = [];
-var esqueletoAndandoJSON, esqueletoAndandoIMG;
+var esqueletoAndandoIMG;
 var bau, bauA, bauFechado;
 var pirata
+var esqueleto
 
 function preload() {
   chao = loadImage("assets/chao.jpg");
-  pirataAndandoIMG = loadImage("assets/pirataA.png");
-  pirataAndandoJSON = loadJSON("assets/pirataA.json");
+  pirataAndandoIMG = loadImage("assets/luffyandando.gif");
+  pirataAtacando = loadImage("assets/luffysoco.gif");
+  esqueletoAndandoIMG = loadImage("assets/Esqueletoandando.gif");
+
   bauFechado = loadImage("assets/Chest01.png");
   bauA = loadAnimation(
     "assets/Chest01.png",
@@ -31,24 +32,18 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   engine = Engine.create();
   world = engine.world;
-  var pirataAndandoFrames = pirataAndandoJSON.frames;
-  for (var i = 0; i < pirataAndandoFrames.length; i++) {
-    var pos = pirataAndandoFrames[i].position;
-    var img = pirataAndandoIMG.get(pos.x, pos.y, pos.w, pos.h);
-    pirataAndando.push(img);
-
-    //pirataAndandoIMG = createSprite(200,300)
+  pirata = Bodies.rectangle(100, height -175, 120, 150)
+  esqueleto = Bodies.rectangle(random(150, width), height -175, 120, 190)
+  
   }
-}
+
 
 function draw() {
   background(chao);
   Engine.update(engine);
-  exibirpiratas()
   drawSprites();
+  image(pirataAndandoIMG, pirata.position.x, pirata.position.y, 120, 150);
+  image(esqueletoAndandoIMG, esqueleto.position.x, esqueleto.position.y, 120, 190);
+
 }
 
-function exibirpiratas(){
-  pirata = new Pirata(50, height-100, 70, 70, pirataAndando)
-  pirata.display()
-}
